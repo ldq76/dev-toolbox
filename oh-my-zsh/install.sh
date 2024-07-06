4c3a9047f93a207229c9b7586eea69ad9089ccb8
Marvine Li <lidaqian11@gmail.com>
2024-07-07 01:51:14 +0800

    feat: install zshrc for humans

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,10 +3,22 @@ set -e
 DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
 sudo apt-get update
-
 sudo apt-get install zsh -y
+if command -v curl >/dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed "/exec zsh -l/d")"
+else
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed "/exec zsh -l/d")"
+fi
 
-# Escape characters in DIR that might interfere with sed
-ESCAPED_DIR=$(echo "$DIR" | sed 's_/_\\/_g')
+# Install spaceship theme
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed "s/exec zsh -l/exec zsh -l -c \". ${ESCAPED_DIR}\/install-plugins.sh\"/g")"
+# Install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions --depth=1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting --depth=1
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM}/plugins/zsh-z --depth=1
+
+cp "$DIR"/.zshrc ~/.zshrc
+exec zsh -l
